391a10931cc134e2f3ee4c67afa49963b072107d
Jing Chen <jing.chen@crypto.com>
2022-07-22 00:00:24 +0800

    Remove debug lib

--- chain/contracts/OptimizedFruitStandV3.sol
+++ chain/contracts/OptimizedFruitStandV3.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
 contract OptimizedWATERV3 is ERC20 {
     constructor(uint256 initialSupply) ERC20("WaterToken", "WATER") {
